bff9ff48220545adc410393a9a69bb25cb2d5a33
anishsingh90 <anishraaz90@gmail.com>
2024-11-16 00:40:12 +0530

    second code

---
+++ Day1/code2.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract SolidityTest{
+    constructor() public {
+
+    }
+    function getResult() public view returns(uint256){
+        uint a = 1;
+        uint b = 2;
+        uint result = a + b;
+    }
+}
