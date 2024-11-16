2b53f7663aa5d12236699a415d94e6070e39397d
anishsingh90 <anishraaz90@gmail.com>
2024-11-17 00:20:36 +0530

    sum

---
+++ Day365(02)/code1.sol
@@ -0,0 +1,15 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityTest{
+    uint public a;
+    uint public b;
+    uint public sum;
+
+    constructor() public  {
+        a = 3;
+        b = 5;
+        sum = a + b;
+    }
+}
