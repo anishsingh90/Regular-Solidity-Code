6a52bc2cff8e522988094e6661a594e0de154baf
anishsingh90 <anishraaz90@gmail.com>
2024-11-17 17:49:47 +0530

    base constructor

--- Day365(02)/base_constructor.sol
+++ Day365(02)/base_constructor.sol
@@ -4,7 +4,7 @@ contract Base {
    uint data;
    constructor(uint _data) public {
       data = _data;   
-   }
+   } 
 }
 contract Derived is Base {
    constructor(uint _info) Base(_info * _info) public {}
