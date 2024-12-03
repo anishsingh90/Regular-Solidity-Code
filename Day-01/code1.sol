50af9ab492eae66eb8109fa7bcebe3c860cd193b
anishsingh90 <anishraaz90@gmail.com>
2024-12-03 18:48:24 +0530

    State variable

--- Day-01/code1.sol
+++ Day-01/code1.sol
@@ -1,11 +1,16 @@
-pragma solidity ^0.5.0;
-contract SolidityTest {
-   constructor() public{
+//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityTest{
+   constructor() public {
+
    }
+
    function getResult() public view returns(uint){
       uint a = 1;
-      uint b = 2;
-      uint result = a + b;
-      return result;
+      uint b = 7;
+      uint c = a + b;
+      return c;
    }
 }
---
+++ Day-01/code2.sol
@@ -0,0 +1,17 @@
+//State Variable
+
+//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityTest{
+    uint storedData; //state variable
+
+    constructor() public  {
+        storedData = 10; //using state varaible
+    }
+
+    function getResult() public view returns(uint){
+        return storedData;
+    }
+}
