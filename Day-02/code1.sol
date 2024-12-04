9e9680ad8fa9c55e7f14aaaf14f7e3128b8b8b78
anishsingh90 <anishraaz90@gmail.com>
2024-12-04 17:33:53 +0530

    variable internal acess

---
+++ Day-02/code1.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract SolidityTest{
+    uint public data = 30;
+    uint internal idata = 10;
+
+    function x() public returns(uint){
+        data = 3; //internal access
+        return data;
+    }
+}
