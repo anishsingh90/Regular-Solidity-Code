8361aa69766086cbdb114e5362cdf571077634f3
anishsingh90 <anishraaz90@gmail.com>
2024-12-05 17:16:24 +0530

    Arithmetic Operator

---
+++ Day-03/code2.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract Addition{
+    constructor() public {
+
+    }
+
+    function getResult() public view returns(uint){
+        uint a = 1;
+        uint b = 2;
+        uint c = a + b;
+        return c;
+    }
+}
