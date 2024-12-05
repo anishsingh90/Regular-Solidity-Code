493ead510fe2c0083ce71faa827c485b5affffa0
anishsingh90 <anishraaz90@gmail.com>
2024-12-05 17:00:11 +0530

    conditional statements

---
+++ Day-03/code1.sol
@@ -0,0 +1,19 @@
+
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Condition{
+    function check(uint a) public view returns(string memory){
+        string memory value;
+
+        if(a >= 18 && a < 30){
+            value = "Adult";
+        } else if(a >= 30){
+            value = "More Adult";
+        } else{
+            value = "Not Adult";
+        }
+        return value;
+    }
+}
