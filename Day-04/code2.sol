c8f957e914b5d7b953223b65c54cb6177ff51ee2
anishsingh90 <anishraaz90@gmail.com>
2024-12-06 18:05:55 +0530

    while loop

---
+++ Day-04/code2.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract SolidityTest{
+    uint storedData; //state variable 
+    constructor() public {
+        storedData = 10;
+    }
+
+    function getResult() public view returns(string memory){
+        uint a = 1; //local variable
+        uint b = 4;
+        uint result = a + b;
+
+        return integerToString(result);
+    }
+
+    function integerToString(uint _i) pure internal returns(string memory){
+        if(_i == 0){
+            return "0";
+        }
+
+        uint j = _i;
+        uint len;
+
+        while(j != 0){
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+
+        while(_i != 0){ //while loop
+        bstr[k--] = byte(uint8(48 + _i % 10));
+         _i /= 10;
+        }
+        return string(bstr);
+    } 
+}
