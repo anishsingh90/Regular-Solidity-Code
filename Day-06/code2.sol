a965a54887a1a24b564a577494b3e515470106ce
anishsingh90 <anishraaz90@gmail.com>
2024-12-10 18:18:14 +0530

    value type

---
+++ Day-06/code2.sol
@@ -0,0 +1,19 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract SolidityTest{
+    bool public boolean = false;
+
+    int32 public int_var = 45;
+
+    string public str = "Geeks For Geeks";
+
+    bytes1 public b = "a";
+
+    enum my_enum{geeks_, _for, geeks}
+
+    function Enum() public pure returns(my_enum){
+        return my_enum.geeks;
+    }
+}
