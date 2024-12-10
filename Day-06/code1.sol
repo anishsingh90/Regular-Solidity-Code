315f8c34a9c3c33803bd19a6daec40b48be48aa2
anishsingh90 <anishraaz90@gmail.com>
2024-12-10 17:17:03 +0530

    types in solidity

---
+++ Day-06/code1.sol
@@ -0,0 +1,13 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.9 <0.9.0;
+
+contract SolidityTest{
+    bool public booleans = false;
+
+    string public Name = "Anish Singh";
+
+    uint public Number = 29;
+
+    bytes public b = "a";
+}
