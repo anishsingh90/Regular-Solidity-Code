ae320465a422f898e1ddf2e2bd955845f77c69fe
anishsingh90 <anishraaz90@gmail.com>
2024-12-11 16:54:24 +0530

    Global Variable

---
+++ Day-07/code1.sol
@@ -0,0 +1,11 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract State_var{
+    address public a;
+
+    constructor() public {
+        a = msg.sender;
+    }
+}
