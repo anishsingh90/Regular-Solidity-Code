079d9fb770d1064e19832288d7bd5b1272213624
anishsingh90 <anishraaz90@gmail.com>
2024-12-11 17:14:29 +0530

    state variable scope

---
+++ Day-07/code3.sol
@@ -0,0 +1,11 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract State_var{
+    uint public num = 0;
+
+    function increase() public{
+        num += 1;
+    }
+}
