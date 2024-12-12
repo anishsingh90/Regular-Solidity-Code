0e1a009c403e65d34d01d8869804da8682d1b737
anishsingh90 <anishraaz90@gmail.com>
2024-12-12 10:25:17 +0530

    else if

---
+++ Day-08/code5.sol
@@ -0,0 +1,17 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract ElseContions{
+    uint i = 10;
+    bool even;
+
+    function get() public returns(bool){
+        if(i % 2 == 0){
+            even = true;
+        }else{
+            even = false;
+        }
+        return even;
+    }
+}
