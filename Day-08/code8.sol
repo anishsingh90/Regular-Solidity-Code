59c7b70398bca12c2226cf98fc1e3ec7fc72408b
anishsingh90 <anishraaz90@gmail.com>
2024-12-12 23:31:25 +0530

    continue statment

---
+++ Day-08/code8.sol
@@ -0,0 +1,20 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Continue_stmnt{
+    uint[] data;
+
+    uint i = 0;
+
+    function get() public returns(uint[] memory){
+        while (i < 10){
+            i++;
+            if(i == 5){
+                continue;
+            }
+            data.push(i);
+        }
+        return data;
+    }
+}
