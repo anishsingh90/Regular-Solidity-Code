2c6f1e219d1a494f037787075b2caf6bce4df433
anishsingh90 <anishraaz90@gmail.com>
2024-12-12 09:14:47 +0530

    while loop

---
+++ Day-08/code1.sol
@@ -0,0 +1,17 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract SolidityTest{
+    uint[] data;
+
+    uint j = 0;
+
+    function loop() public returns(uint[] memory){
+        while(j<5){
+            j++;
+            data.push(j);
+        }
+        return data;
+    }
+}
