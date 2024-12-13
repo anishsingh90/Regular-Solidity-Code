e56d08c7ce39b6ec0d4a0716023d7998aacafee4
anishsingh90 <anishraaz90@gmail.com>
2024-12-13 15:07:48 +0530

    set string by function

---
+++ Day-09/code2.sol
@@ -0,0 +1,19 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract SolidityTest{
+    string text;
+
+    function setText() public {
+        text = "Anish";
+    }
+
+    function setTextByPassingValue(string memory message) public {
+        text = message;
+    }
+
+    function getString() public view returns(string memory){
+        return text;
+    }
+}
