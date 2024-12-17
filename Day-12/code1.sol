3270d9024d38305b37a324a3ece860a2be60bf2d
anishsingh90 <anishraaz90@gmail.com>
2024-12-17 23:50:53 +0530

    fall back function

---
+++ Day-12/code1.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0 
+pragma solidity >=0.8.0 <0.9.0; 
+/// @title A contract for demonstrate fallback function
+/// @author Jitendra Kumar
+/// @notice For now, this contract just show how to fallback function receive all the ether 
+contract GeeksForGeeks 
+{
+	string public calledFallbackFun;
+
+	// This fallback function 
+	// will keep all the Ether
+	fallback() external payable{
+		calledFallbackFun="Fallback function is executed!";
+	}
+
+	function getBalance() public view returns (uint) {
+		return address(this).balance;
+	}
+} 
+
+// Creating the sender contract
+contract Sender 
+{
+	function transferEther() public payable 
+	{
+		//paste the deployed contract address of GeeksForGeeks smart contract here
+		(bool sent, ) = payable(0xD4Fc541236927E2EAf8F27606bD7309C1Fc2cbee).call{value: 2 ether}("Transaction Completed!");
+		require(sent, "Transaction Failed!");
+	}
+
+	function getBalance() public view returns (uint) {
+		return address(this).balance;
+	}
+}
