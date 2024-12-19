2a915b0aed0e07ff2ffa86297e0cd3182ff37035
anishsingh90 <anishraaz90@gmail.com>
2024-12-19 17:34:06 +0530

    Enum

--- Day-14/code1.sol
+++ Day-14/code1.sol
@@ -1,27 +1,30 @@
-pragma solidity ^0.4.18; 
+//SPDX-Licence-Identifier: GPL-3.0
 
-contract mapping_example {
-	 
-	struct student {
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
-		string name;
-		string subject;
-		uint8 marks;
+contract SolidityTest{
+	//create the enumurator
+	enum Student_Info{
+		Name,
+		Course,
+		Department
 	}
 
-	mapping (address => student) result;
-	address[] public student_result;
-	
-	
-	function adding_values() public {
-		var student = result[0xDEE7796E89C82C36BAdd1375076f39D69FafE252];
+	Student_Info Info;
+	Student_Info Unq_Info;
 
-		student.name = "Anish Singh";
-		student.subject = "Operating System";
-		student.marks = 45; //out of 100
-		student_result.push(
-		0xDEE7796E89C82C36BAdd1375076f39D69FafE252) -1;
+	Student_Info constant default_value = Student_Info.Name;
 
+	function set_value() public {
+		Unq_Info = Student_Info.Course;
 	}
-	
+
+	function get_Inq() public view returns(Student_Info){
+		return Unq_Info;
+	}
+
+	function get_value() public view returns(Student_Info){
+		return default_value;
+	}
 }
