7ccf7dfd17899ec6d3ebc45734887bc9c6ca1740
anishsingh90 <anishraaz90@gmail.com>
2024-12-26 15:40:27 +0530

    sd

--- Day-03/code1.sol
+++ Day-03/code1.sol
@@ -6,7 +6,6 @@ pragma solidity >=0.5.0 <0.9.0;
 contract Condition{
     function check(uint a) public view returns(string memory){
         string memory value;
-
         if(a >= 18 && a < 30){
             value = "Adult";
         } else if(a >= 30){
