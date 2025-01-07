a800858c30a7b38f4ef240d05bc0740e6075f4f8
anishsingh90 <anishraaz90@gmail.com>
2025-01-07 17:50:08 +0530

    fallback function

---
+++ Day-17/code1.sol
@@ -0,0 +1,28 @@
+//SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SolidityTest{
+    string public calledFallbackFun;
+
+    fallback() external payable {
+        calledFallbackFun = "Fallback function is executed";
+     }
+
+     function getBalance() public view returns(uint){
+        return address(this).balance;
+     }
+}
+
+contract Sender{
+    function transferEther() public payable {
+        (bool sent, ) = payable(0x5d5107BDa101F69c371993820478612672A5389e).call{value: 2 ether}("Transaction Completed!");
+        require(sent, "Transaction Failed");
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+}
+
