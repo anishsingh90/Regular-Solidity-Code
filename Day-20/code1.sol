43b2673f03faed5fb8cc817f683a7817ef96b7f6
anishsingh90 <anishraaz90@gmail.com>
2025-01-10 17:11:32 +0530

    Inheritance in Solidity

---
+++ Day-20/code1.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract A{
+    function fun1() public pure returns(string memory){
+        return "I'm contract A";
+    }
+    function fun2() public pure returns(string memory){
+        return "I'm contract A";
+    }
+    function fun3() public pure virtual returns(string memory){
+        return "I'm contract A";
+    }
+    function fun4() public pure virtual returns(string memory){
+        return "I'm contract A";
+    }
+}
+
+contract B is A{
+    function fun3() public pure override returns(string memory){
+        return "I'm contract B";
+    }
+    function fun4() public pure override returns(string memory){
+        return "I'm contract B";
+    }
+}
