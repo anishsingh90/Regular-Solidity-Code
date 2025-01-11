88664714899617af6d0d1d585cdafd20fb57bc97
anishsingh90 <anishraaz90@gmail.com>
2025-01-11 19:00:04 +0530

    Interface in solidity

---
+++ Day-23/code1.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+interface Member {
+    function setName() external returns(string memory);
+    function setAge() external returns(uint);
+}
+
+contract Techer is Member{
+    function setName() public pure returns(string memory){
+        return "Anish";
+    }
+
+    //implement the requiered 'setAge()' here
+    function setAge() public pure returns(uint){
+        return 20;
+    }
+}
