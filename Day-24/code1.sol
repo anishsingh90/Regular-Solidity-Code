8595d35f51acf4c2a637fc89aefab2e75250fc4f
anishsingh90 <anishraaz90@gmail.com>
2025-01-11 22:20:15 +0530

    inheritance

---
+++ Day-24/code1.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Indentifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract A{
+    function fun1() public pure returns(string memory){
+        return "I am contract A";
+    }
+
+    function fun2() public pure virtual returns(string memory){
+        return "I am contract A";
+    }
+
+    function fun3() public pure returns(string memory){
+        return "I am contract A";
+    }
+}
+
+contract B is A{
+    function fun2() public pure override returns(string memory){
+        return "I am contract B";
+    }
+}
