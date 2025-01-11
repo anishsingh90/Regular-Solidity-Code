d686d72039c8a7ef00a95bd781aa1f9c322c3dd2
anishsingh90 <anishraaz90@gmail.com>
2025-01-11 18:05:11 +0530

    abstract contract

---
+++ Day-22/code2.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+abstract contract AbstractContract{
+    function getStr(string memory _strIn) public view virtual returns(string memory);
+}
+
+contract DerivedContract is AbstractContract{
+    function getStr(string memory _strIn) public pure override returns(string memory){
+        return _strIn;
+    }
+}
