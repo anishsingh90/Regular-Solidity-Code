625f3349eeb1074bbd209e52585a001034f4b1a5
anishsingh90 <anishraaz90@gmail.com>
2025-01-13 17:42:38 +0530

    library declaration code

---
+++ Day-26/code2.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+library LibraryExample{
+    struct Constanst{
+        uint pi;
+        uint id;
+        uint salary;
+        string name;
+    }
+}
