dde5724105f128dd877eabefd4bb0d9b74d7e360
anishsingh90 <anishraaz90@gmail.com>
2025-01-16 17:24:42 +0530

    print hello world

---
+++ Day-28/code2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract HelloWorld{
+    string message;
+
+    constructor() public {
+        message = "Hello World!";
+    }
+
+    function get() public view returns(string memory){
+        return message;
+    }
+}
