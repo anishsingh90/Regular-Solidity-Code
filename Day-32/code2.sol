ede99d20e64399f4f73fbfbc089fcd29d84dc44d
anishsingh90 <anishraaz90@gmail.com>
2025-01-22 17:28:34 +0530

    print hello world

---
+++ Day-32/code2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityTest{
+    string public message;
+
+    function set(string memory value) public {
+    message = value;
+    }
+}
