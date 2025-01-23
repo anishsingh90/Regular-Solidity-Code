4a62caf7f38d1ff6e7bfea64ba4c8154b4b9f29a
anishsingh90 <anishraaz90@gmail.com>
2025-01-23 17:25:49 +0530

    hello

---
+++ Day-34/code1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityTest{
+    uint public state_var;
+
+    constructor() public {
+        state_var = 113;
+    }
+}
