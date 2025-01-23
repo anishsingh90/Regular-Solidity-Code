dd2a3a5ca1edcd0d64f61b9896e9d054d441c76e
anishsingh90 <anishraaz90@gmail.com>
2025-01-23 09:15:18 +0530

    constructor

---
+++ Day-33/code1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Counter{
+    uint public counter;
+
+    constructor(uint _counter){
+        counter = _counter;
+    }
+}
