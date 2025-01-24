40112e9bd7b9a3ecb80633f0f87ba3453c83cdbb
anishsingh90 <anishraaz90@gmail.com>
2025-01-24 17:48:03 +0530

    else if

---
+++ Day-35/code6.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityTest{
+    uint i = 10;
+
+    function get() public view returns(string memory){
+        if(i < 10){
+            return "Smaller than 10";
+        }else if(i == 10){
+            return "Equal to 10";
+        }else{
+            return "Greater than 10";
+        }
+    }
+}
