ef0506b1dee1a3e0e0b2587aa446b354316999cd
anishsingh90 <anishraaz90@gmail.com>
2025-01-27 01:32:39 +0530

    set the value in array

--- Day-36/code2.sol
+++ Day-36/code2.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract SolidityTest{
-    uint[4] public arr = [1,2,3,4];
+contract Array{
+    uint[4] public arr = [1, 2, 3, 4];
+
+    function setter(uint index, uint value) public {
+        arr[index] = value;
+    }
 }
