8de3f75067ef021eb739558b976a036aa7025bfb
anishsingh90 <anishraaz90@gmail.com>
2025-02-03 23:03:57 +0530

    string

---
+++ Day-38/code1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityTest{
+    string str;
+
+    function Str() public returns(string memory){
+        str = "Anish";
+
+        return str;
+    }
+}
